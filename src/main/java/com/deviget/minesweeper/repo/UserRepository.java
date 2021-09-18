0b9ab8070c080282a20e5560b2775b1ec616d905
Josmar Soares de Souza Junior <josmar.souzajr@gmail.com>
2021-09-18 16:27:53 -0300

    First commit using spring-boot-java-rest-api-archetype

---
+++ src/main/java/com/deviget/minesweeper/repo/UserRepository.java
@@ -0,0 +1,10 @@
+package com.deviget.minesweeper.repo;
+
+import com.deviget.minesweeper.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
